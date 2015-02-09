6dc29f07f7c222f54825815154a7b7ef5e8f393a
Nick Mosher <codewhisperer97@gmail.com>
2015-02-09 14:00:43 -0500

    Added some skeleton code for Jaw.

--- src/org/usfirst/frc/team1829/robot/subsystems/Jaw.java
+++ src/org/usfirst/frc/team1829/robot/subsystems/Jaw.java
@@ -15,15 +15,41 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Jaw extends Subsystem
 {
+	/**
+	 * Motor that pushes and retracts the Jaw linkage
+	 * to compress the CONTAINER.
+	 */
 	private Talon jawMotor;
-	private Talon feedMotor;
-	private CarbonDigitalInput jawLimit;
 	
+	/**
+	 * Motor that drives the gripping rollers that move
+	 * CONTAINERS from the Jaw to the Conveyor.
+	 */
+	private Talon feedMotor;
+	
+	/**
+	 * Limit switch that triggers when the Jaw is in its
+	 * fully retracted position.
+	 */
+	private CarbonDigitalInput retractLimit;
+	
+	/**
+	 * Limit switch that triggers when the Jaw has
+	 * encountered a new CONTAINER (i.e. the robot
+	 * has driven the Jaw into contact with a
+	 * CONTAINER).
+	 */
+	private CarbonDigitalInput encounterLimit;
+	
+	/**
+	 * Constructs the Jaw subsystem.
+	 */
 	public Jaw()
 	{
 		jawMotor = new Talon(Robot.JAW_GRAB_MOTOR);
 		feedMotor = new Talon(Robot.JAW_FEED_MOTOR);
-		jawLimit = new CarbonDigitalInput(Robot.JAW_LIMIT);
+		retractLimit = new CarbonDigitalInput(Robot.JAW_LIMIT_RETRACT);
+		encounterLimit = new CarbonDigitalInput(Robot.JAW_LIMIT_ENCOUNTER);
 	}
 	
 	@Override
